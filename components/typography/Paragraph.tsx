2cbce338f02020e515679cfe7317a6b7340891e8
LinCie <aldiofernanda@protonmail.com>
2023-11-03 17:29:52 +0700

    update padding and remove indent

--- components/typography/Paragraph.tsx
+++ components/typography/Paragraph.tsx
@@ -8,7 +8,7 @@ interface Props extends HTMLAttributes<HTMLParagraphElement> {
 export default function Paragraph({ className, children, ...props }: Props) {
   return (
     <p
-      className={`pb-2 text-justify indent-5 text-sm text-black dark:text-white sm:text-base sm:indent-8 ${className}`.trim()}
+      className={`mb-2 text-sm text-black dark:text-white sm:text-base ${className}`.trim()}
       {...props}
     >
       {children}
