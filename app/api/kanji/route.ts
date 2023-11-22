04135a427c54055ed5c9e480542e715de6e984fa
LinCie <aldiofernanda@protonmail.com>
2023-11-22 11:16:56 +0700

    adjust EN search logic

--- app/api/kanji/route.ts
+++ app/api/kanji/route.ts
@@ -1,4 +1,4 @@
-import { kanjidic } from "@/components/dictionaries/kanjidic";
+import { KanjiCharacter, kanjidic } from "@/components/dictionaries/kanjidic";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
@@ -16,14 +16,39 @@ export async function GET(req: Request) {
       );
     }
 
-    const characters = [];
+    const characters: KanjiCharacter[] = [];
 
     const language: string | null = params.get("lang");
 
     // Do this if the user search by using english
     if (language === "en") {
       // The search result
-      const searchResult = kanjidic.characters
+      const searchResult: KanjiCharacter[] = [];
+
+      // Find by Similarity
+      const sortSimilar = kanjidic.characters
+        // Filter by finding characters that has the same or contains the search result in its meanings
+        .filter(
+          (character) =>
+            character.readingMeaning?.groups?.some(
+              (group) =>
+                group.meanings?.some(
+                  (m) => m.value.toLowerCase() === search.toLowerCase(),
+                ),
+            ),
+        )
+        // Sort the search result in ascending order by order of frequency
+        .sort(
+          (a, b) => (a.misc?.frequency || 9999) - (b.misc?.frequency || 9999),
+        );
+
+      // If sortSimilar found similarity, push into characters array
+      if (sortSimilar.length > 0) {
+        sortSimilar.forEach((character) => searchResult.push(character));
+      }
+
+      // Find if it includes the searched meaning
+      const sortIncludes = kanjidic.characters
         // Filter by finding characters that has the same or contains the search result in its meanings
         .filter(
           (character) =>
@@ -37,13 +62,23 @@ export async function GET(req: Request) {
         // Sort the search result in ascending order by order of frequency
         .sort(
           (a, b) => (a.misc?.frequency || 9999) - (b.misc?.frequency || 9999),
-        )
-        // Slice search result to only show 5 most frequently used kanji
-        .slice(0, 5);
+        );
 
-      // Push each character into characters array
-      if (searchResult) {
-        searchResult.forEach((character) => characters.push(character));
+      // If sortIncludes found kanji that includes the meaning
+      if (sortIncludes.length > 0) {
+        // Push if searchResult does not contain the character, dont push otherwise
+        sortIncludes.forEach((character) => {
+          if (!searchResult.includes(character)) {
+            searchResult.push(character);
+          }
+        });
+      }
+
+      // Slice so it contains 5 items only, and then push each character into characters array
+      if (searchResult.length > 0) {
+        searchResult
+          .slice(0, 5)
+          .forEach((character) => characters.push(character));
       }
 
       // Do this if user search by using Japanese
@@ -77,7 +112,7 @@ export async function GET(req: Request) {
           .slice(0, 5);
 
         // Push each character into characters array
-        if (searchResult) {
+        if (searchResult.length > 0) {
           searchResult.forEach((character) => characters.push(character));
         }
         // Do this if user doesn't provide "by" parameter
