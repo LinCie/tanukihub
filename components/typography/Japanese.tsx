5080812bffcd6ff8126b4bcd2f8611d3a3e16cf3
LinCie <aldiofernanda@protonmail.com>
2023-12-25 20:44:12 +0700

    revert nowrap

--- components/typography/Japanese.tsx
+++ components/typography/Japanese.tsx
@@ -8,7 +8,7 @@ export default function Japanese({ children, className, ...props }: Props) {
   const customClass = className ? className : "";
 
   return (
-    <span className={`font-zen whitespace-nowrap ${customClass}`.trim()} {...props}>
+    <span className={`font-zen ${customClass}`.trim()} {...props}>
       {children}
     </span>
   );
