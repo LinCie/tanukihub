65ad3ae49e0ddd43ea4ffc981d6088a4e8b3f159
LinCie <aldiofernanda@protonmail.com>
2023-12-30 13:30:02 +0700

    adjust paddings

--- components/typography/Paragraph.tsx
+++ components/typography/Paragraph.tsx
@@ -9,7 +9,7 @@ export default function Paragraph({ className, children, ...props }: Props) {
 
   return (
     <p
-      className={`text-main-dark dark:text-main-light mb-2 indent-3 text-sm last-of-type:mb-4 sm:mb-3 sm:indent-5 sm:text-base last-of-type:sm:mb-5 ${customClass}`.trim()}
+      className={`text-main-dark dark:text-main-light mb-2 indent-3 text-sm sm:mb-3 sm:indent-5 sm:text-base ${customClass}`.trim()}
       {...props}
     >
       {children}
