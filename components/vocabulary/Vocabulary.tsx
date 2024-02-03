991c315d2c185515ea39bef19bba56907b823c9f
LinCie <aldiofernanda@protonmail.com>
2024-02-03 16:00:37 +0700

    add border

--- components/vocabulary/Vocabulary.tsx
+++ components/vocabulary/Vocabulary.tsx
@@ -32,12 +32,31 @@ export default function Vocabulary({
       )}
       {...props}
     >
-      <h3 className="mb-2 flex items-center gap-1 text-sm font-bold text-main-title-dark dark:text-main-title-light sm:mb-3 sm:gap-2 sm:text-base">
+      <div
+        className={cn(
+          // Base styles
+          "mb-2 flex items-center gap-1 border-b-2 pb-1 text-sm font-bold",
+
+          // Light mode classes
+          "border-main-identity/75 text-main-title-dark",
+
+          // Dark mode classes
+          "dark:border-main-title-light/75 dark:text-main-title-light",
+
+          // Small screen breakpoint
+          "sm:mb-3 sm:gap-2 sm:text-base",
+        )}
+      >
         <div>
-          <InfoCircledIcon className="size-4 text-main-title-dark dark:text-main-title-light sm:size-5" />
+          <InfoCircledIcon
+            className={cn(
+              // Base styles
+              "size-4 text-main-title-dark dark:text-main-title-light sm:size-5",
+            )}
+          />
         </div>
-        <div>Vocabulary</div>
-      </h3>
+        <h3>Vocabulary</h3>
+      </div>
       <div className="mb-2 text-sm sm:mb-3 sm:text-base">
         Note: you can click on the kanji to get the full meanings
       </div>
